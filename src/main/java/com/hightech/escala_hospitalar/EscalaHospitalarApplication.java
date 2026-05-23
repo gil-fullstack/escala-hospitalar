96530325c3a3283d9ec6971ea1dc823fbdc2620e
gil-fullstack <gilservicosweb@gmail.com>
2026-05-23 11:27:31 -0300

    first commit

---
+++ src/main/java/com/hightech/escala_hospitalar/EscalaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package com.hightech.escala_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaHospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hightech/escala_hospitalar/EscalaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hightech.escala_hospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
